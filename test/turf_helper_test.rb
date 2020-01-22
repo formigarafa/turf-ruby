cde29d86e0e256175cd6862d5c8529ef8c9f3753
Rafael Santos <formigarafa@gmail.com>
2020-01-22 14:02:41 +1300

    polygon next

--- test/turf_helper_test.rb
+++ test/turf_helper_test.rb
@@ -32,14 +32,7 @@ class TurfHelperTest < Minitest::Test
     end
   end
 
-  def test_point
-    pt_array = Turf.point([5, 10], name: "test point")
-
-    assert_equal(pt_array[:geometry][:coordinates][0], 5)
-    assert_equal(pt_array[:geometry][:coordinates][1], 10)
-    assert_equal(pt_array[:properties][:name], "test point")
-
-    no_props = Turf.point([0, 0])
-    assert_equal(no_props[:properties], {}, "no props becomes {}")
+  def test_polygon
+    assert false
   end
 end
