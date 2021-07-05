d3134d4ce957a35d3f6de2788c719b3ce07a8103
Michael Yin <me@micy.in>
2021-07-05 23:10:35 +1200

    turf_area_test: round result before assertion

--- test/turf_area_test.rb
+++ test/turf_area_test.rb
@@ -5,12 +5,12 @@ require "test_helper"
 class TurfAlongTest < Minitest::Test
   def test_area
     [
-      ["polygon", 7_766_240_997_209.015],
-      ["polygon_holes", 101_998_969_915.32196],
-      ["multi_polygon", 20_310_537_131.032814],
+      ["polygon", 7_766_240_997_209],
+      ["polygon_holes", 101_998_969_915],
+      ["multi_polygon", 20_310_537_131],
     ].each do |name, area|
       input = load_geojson "area/#{name}.geojson"
-      assert_equal area, Turf.area(input)
+      assert_equal area, Turf.area(input).round
     end
   end
 end
