fa5c1b0536db2e70137d33e3daf3de249de298fe
Rafael Santos <formigarafa@gmail.com>
2025-03-16 15:26:08 +1300

    update/fix centroid_test

--- test/turf_centroid_test.rb
+++ test/turf_centroid_test.rb
@@ -3,22 +3,42 @@
 require "test_helper"
 # @see https://github.com/Turfjs/turf/blob/master/packages/turf-centroid/test.js
 class TurfCentroidTest < Minitest::Test
-  # feature-collection
-  # linestring
-  # point
-  # imbalanced-polygon
-  %w[
-    polygon
-  ].each do |name|
-    define_method "test_centroid_#{name}" do
-      geojson = load_geojson "centroid/in/#{name}.geojson"
-      out = load_geojson "centroid/out/#{name}.geojson", symbolize_names: true
-      centered = Turf.centroid geojson, { "marker-symbol": "circle" }
-      results = Turf.feature_collection [centered]
-      Turf.feature_each geojson do |feature|
-        results[:features].push feature
-      end
-      assert_equal(out, results)
+  def setup
+    @directories = {
+      in: File.join(__dir__, 'centroid', 'in'),
+      out: File.join(__dir__, 'centroid', 'out')
+    }
+    @fixtures = Dir.glob(File.join(@directories[:in], '*.geojson')).map do |input|
+      {
+        name: File.basename(input, File.extname(input)),
+        filename: File.basename(input),
+        geojson: JSON.parse(File.read(input), symbolize_names: true),
+        out: File.join(@directories[:out], File.basename(input))
+      }
     end
+  end
+
+  def test_centroid
+    @fixtures.each do |fixture|
+      name = fixture[:name]
+      geojson = fixture[:geojson]
+      out = fixture[:out]
+      centered = Turf.centroid(geojson, { 'marker-symbol': 'circle' })
+      result = Turf.feature_collection([centered])
+      Turf.feature_each(geojson) { |feature| result[:features] << feature }
+
+      assert_equal JSON.parse(File.read(out), symbolize_names: true), result, name
+    end
+  end
+
+  def test_centroid_properties
+    line = Turf.line_string(
+      [
+        [0, 0],
+        [1, 1]
+      ]
+    )
+    pt = Turf.centroid(line, { foo: 'bar' })
+    assert_equal 'bar', pt.dig(:properties, :foo), 'translate properties'
   end
 end
