4a16c9adf5db386e9deb1c2884928138f6d4bda0
Rafael Santos <formigarafa@gmail.com>
2025-04-19 11:32:24 +1200

    bbox blank tests

---
+++ test/turf_bbox_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TurfBboxTest < Minitest::Test
+  def test_bbox
+  end
+
+  def test_bbox_throws
+  end
+
+  def test_null_geometries
+  end
+end
