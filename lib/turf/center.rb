50796f0c67f50f48f28d729b59c42b9a8e7f349a
Rafael Santos <formigarafa@gmail.com>
2025-04-20 17:18:26 +1200

    port center

--- lib/turf/center.rb
+++ lib/turf/center.rb
@@ -2,7 +2,31 @@
 
 # :nodoc:
 module Turf
-  def center(*args)
-    raise NotImplementedError
+  # frozen_string_literal: true
+
+  # Takes a Feature or FeatureCollection and returns the absolute center point of all features.
+  #
+  # @param [GeoJSON] geojson GeoJSON to be centered
+  # @param [Hash] options Optional parameters
+  # @option options [Hash] :properties Translate GeoJSON Properties to Point
+  # @option options [Array] :bbox Translate GeoJSON BBox to Point
+  # @option options [String, Integer] :id Translate GeoJSON Id to Point
+  # @return [Feature<Point>] a Point feature at the absolute center point of all input features
+  # @example
+  #   features = points([
+  #     [-97.522259, 35.4691],
+  #     [-97.502754, 35.463455],
+  #     [-97.508269, 35.463245]
+  #   ])
+  #   center = center(features)
+  #   # Add to map
+  #   add_to_map = [features, center]
+  #   center[:properties]['marker-size'] = 'large'
+  #   center[:properties]['marker-color'] = '#000'
+  def center(geojson, options = {})
+    ext = bbox(geojson)
+    x = (ext[0] + ext[2]) / 2.0
+    y = (ext[1] + ext[3]) / 2.0
+    point([x, y], options[:properties] || {}, options)
   end
 end
--- test/turf_center_test.rb
+++ test/turf_center_test.rb
@@ -8,6 +8,7 @@ class TurfCenterTest < Minitest::Test
       in: File.join(__dir__, "center", "in"),
       out: File.join(__dir__, "center", "out")
     }
+
     @fixtures = Dir.glob(File.join(@directories[:in], "*.geojson")).map do |input|
       {
         name: File.basename(input, File.extname(input)),
@@ -19,8 +20,42 @@ class TurfCenterTest < Minitest::Test
   end
 
   def test_turf_center
+    @fixtures.each do |fixture|
+      geojson = fixture[:geojson]
+      options = geojson[:options] || {}
+      options[:properties] = { "marker-symbol": "star", "marker-color": "#F00" }
+      centered = Turf.center(geojson, options)
+
+      # Display Results
+      results = Turf.feature_collection([centered])
+      Turf.feature_each(geojson) { |feature| results[:features] << feature }
+
+      extent = Turf.bbox_polygon(Turf.bbox(geojson))
+      extent[:properties] = { stroke: "#00F", "stroke-width": 1, "fill-opacity": 0 }
+      Turf.coord_each(extent) do |coord|
+        results[:features] << Turf.line_string(
+          [coord, centered[:geometry][:coordinates]],
+          {
+            stroke: "#00F",
+            "stroke-width": 1
+          },
+        )
+      end
+      results[:features] << extent
+
+      out_path = File.join(@directories[:out], fixture[:filename])
+      expected = JSON.parse(File.read(out_path), symbolize_names: true)
+      assert_equal(expected, results, "Testing #{fixture[:name]}")
+    end
   end
 
   def test_turf_center_properties
+    line = Turf.line_string([
+      [0, 0],
+      [1, 1],
+    ])
+    pt = Turf.center(line, properties: { foo: "bar" })
+
+    assert_equal("bar", pt[:properties][:foo], "translate properties")
   end
 end
