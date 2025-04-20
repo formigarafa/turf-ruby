d4ef627771cf4ab530c95e63827b8dd761c89286
Rafael Santos <formigarafa@gmail.com>
2025-04-20 17:35:46 +1200

    port square

--- lib/turf/square.rb
+++ lib/turf/square.rb
@@ -2,7 +2,41 @@
 
 # :nodoc:
 module Turf
-  def square(*args)
-    raise NotImplementedError
+  # frozen_string_literal: true
+
+  # Takes a bounding box and calculates the minimum square bounding box that
+  # would contain the input.
+  #
+  # @param [Array<Float>] bbox extent in [west, south, east, north] order
+  # @return [Array<Float>] a square surrounding `bbox`
+  # @example
+  #   bbox = [-20, -20, -15, 0]
+  #   squared = square(bbox)
+  #
+  #   #addToMap
+  #   # add_to_map = [bbox_polygon(bbox), bbox_polygon(squared)]
+  def square(bbox)
+    west, south, east, north = bbox
+
+    horizontal_distance = distance([west, south], [east, south])
+    vertical_distance = distance([west, south], [west, north])
+
+    if horizontal_distance >= vertical_distance
+      vertical_midpoint = (south + north) / 2.0
+      [
+        west,
+        vertical_midpoint - ((east - west) / 2.0),
+        east,
+        vertical_midpoint + ((east - west) / 2.0),
+      ]
+    else
+      horizontal_midpoint = (west + east) / 2.0
+      [
+        horizontal_midpoint - ((north - south) / 2.0),
+        south,
+        horizontal_midpoint + ((north - south) / 2.0),
+        north,
+      ]
+    end
   end
 end
--- test/turf_square_test.rb
+++ test/turf_square_test.rb
@@ -4,5 +4,16 @@ require "test_helper"
 
 class TurfSquareTest < Minitest::Test
   def test_square
+    # Bounding boxes to be tested
+    bbox1 = [0, 0, 5, 10]
+    bbox2 = [0, 0, 10, 5]
+
+    # Call the square function from the Turf module
+    sq1 = Turf.square(bbox1)
+    sq2 = Turf.square(bbox2)
+
+    # Assertions to check if the results match the expected values
+    assert_equal([-2.5, 0, 7.5, 10], sq1, "Square function should properly calculate the square for bbox1")
+    assert_equal([0, -2.5, 10, 7.5], sq2, "Square function should properly calculate the square for bbox2")
   end
 end
