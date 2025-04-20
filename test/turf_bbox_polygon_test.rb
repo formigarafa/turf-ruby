c09e13b5c1e46d6bfed15c04234c1c5771f6c850
Rafael Santos <formigarafa@gmail.com>
2025-04-20 14:44:42 +1200

    bbox polygon blank tests

---
+++ test/turf_bbox_polygon_test.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TurfBBoxPolygonTest < Minitest::Test
+  def test_bbox_polygon
+  end
+
+  def test_bbox_polygon_valid_geojson
+  end
+
+  def test_bbox_polygon_handling_string_input_output
+  end
+
+  def bbox_polygon_error_handling
+  end
+
+  def bbox_polygon_translate_bbox_issue_1179
+  end
+
+  def bbox_polygon_assert_bbox
+  end
+end
