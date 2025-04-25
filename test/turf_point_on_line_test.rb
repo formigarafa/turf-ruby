58feb15cff98b83bb7b2b4dda2be4bbee8702703
Rafael Santos <formigarafa@gmail.com>
2025-04-25 22:42:02 +1200

    blank tests for boolean_point_on_line

---
+++ test/turf_point_on_line_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TurfBooleanPointOnLineTest < Minitest::Test
+  def test_turf_boolean_point_on_line
+  end
+
+  def test_turf_boolean_point_on_line_issue_2750
+  end
+end
