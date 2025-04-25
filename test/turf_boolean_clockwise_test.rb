e87204cd019ca97596afd9eb079016d2dd6aa443
Rafael Santos <formigarafa@gmail.com>
2025-04-25 22:03:40 +1200

    blank tests for boolean_clockwise

---
+++ test/turf_boolean_clockwise_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TurfBooleanClockwiseTest < Minitest::Test
+  def test_is_clockwise_fixtures
+  end
+
+  def test_is_clockwise
+  end
+
+  def test_is_clockwise_geometry_types
+  end
+end
