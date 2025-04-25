1ac1b3b4a37d351074e7ae0ea0fa6332b4e19523
Rafael Santos <formigarafa@gmail.com>
2025-04-25 22:29:33 +1200

    blank tests for boolean_concave

---
+++ test/turf_boolean_concave_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TurfBooleanConcaveTest < Minitest::Test
+  def test_is_concave_fixtures
+  end
+
+  def test_is_concave_geometry_types
+  end
+end
